4468d8ac326249c009be5da13ddf6c58b59d7ba3
Your Name <you@example.com>
2025-01-07 00:39:01 +0500

    Added animation on set operation

--- app/src/main/java/com/example/onemorecalculator/MainActivity.kt
+++ app/src/main/java/com/example/onemorecalculator/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.onemorecalculator
 
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AlphaAnimation
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
@@ -10,12 +11,12 @@ import androidx.activity.ComponentActivity
 class MainActivity : ComponentActivity() {
 
     private lateinit var calc: Calculator
-    private lateinit var text_view: TextView
+    private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        text_view = findViewById<TextView>(R.id.resultTv)
+        textView = findViewById<TextView>(R.id.resultTv)
         calc = Calculator(10)
     }
 
@@ -24,54 +25,64 @@ class MainActivity : ComponentActivity() {
         val button = view as Button
         val number = button.getText().toString()
         calc.input(number)
-        text_view.text = calc.getScreenNumber()
+        textView.text = calc.getScreenNumber()
     }
 
     fun onAC(view: View) {
         view.performHapticFeedback(1)
         calc.reset()
-        text_view.text = calc.getScreenNumber()
+        textView.text = calc.getScreenNumber()
     }
 
     fun onBackspace(view: View) {
         view.performHapticFeedback(1)
         calc.backspace()
-        text_view.text = calc.getScreenNumber()
+        textView.text = calc.getScreenNumber()
     }
 
     fun onInputPi(view: View) {
         view.performHapticFeedback(1)
         calc.inputPi()
-        text_view.text = calc.getScreenNumber()
+        textView.text = calc.getScreenNumber()
     }
 
     fun onAdd(view: View) {
         view.performHapticFeedback(1)
         calc.setOperation(Operation.ADDITION)
-        text_view.text = calc.getScreenNumber()
+        textView.text = calc.getScreenNumber()
+        runTextViewAnimation()
     }
 
     fun onSubtract(view: View) {
         view.performHapticFeedback(1)
         calc.setOperation(Operation.SUBTRACTION)
-        text_view.text = calc.getScreenNumber()
+        textView.text = calc.getScreenNumber()
+        runTextViewAnimation()
     }
 
     fun onMultiply(view: View) {
         view.performHapticFeedback(1)
         calc.setOperation(Operation.MULTIPLICATION)
-        text_view.text = calc.getScreenNumber()
+        textView.text = calc.getScreenNumber()
+        runTextViewAnimation()
     }
 
     fun onDivide(view: View) {
         view.performHapticFeedback(1)
         calc.setOperation(Operation.DIVISION)
-        text_view.text = calc.getScreenNumber()
+        textView.text = calc.getScreenNumber()
+        runTextViewAnimation()
     }
 
     fun onEquals(view: View) {
         view.performHapticFeedback(1)
         calc.equals()
-        text_view.text = calc.getScreenNumber()
+        textView.text = calc.getScreenNumber()
+    }
+
+    private fun runTextViewAnimation() {
+        val fadeOut = AlphaAnimation(1.0f, 0.0f)
+        fadeOut.duration = 100
+        textView.startAnimation(fadeOut)
     }
 }
